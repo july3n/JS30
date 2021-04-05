0e373e28dca90d511787a4941422dbf77460973d
murat <muratorhun@yahoo.com>
2021-04-05 03:11:07 +0300

    add main

---
+++ 03 - CSS Variables/app.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll(".controls input");
+
+const handleUpdate = function () {
+  // console.log(this.value);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+};
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
